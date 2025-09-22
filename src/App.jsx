3462df9d061de8fc4e24e51f745adc2c38f8e410
Jorge Luís <jorgelmlp2@gmail.com>
2025-09-22 20:25:19 -0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import './App.css'
-import MyHeader from './components/MYHeader'
+import './App.css';
+import MyHeader from './components/MyHeader'
+import homeImage from './assets/images/homeImage.png';
+
 
 function App() {
 
   return (
+    
     <div id='main'>
+      <div id='home'>
         <MyHeader></MyHeader>
         <div id='homeText'>
             <h1>HELLO WORLD</h1>
         </div>
+          <div id='homeImage' className='absolute top-15'>
+          <img src={homeImage} alt="" />
+
+        </div>
         <div className='homeid'>
             
         </div>
+    </div>
     </div>
   )
 }
--- src/components/MYHeader/index.jsx
+++ src/components/MYHeader/index.jsx
@@ -1,11 +1,17 @@
 import './style.css'
-
-
-const MyHeader = () =>{
-    return(
-        <header>
-            <div>asdasdasdsad</div>
-        </header>
-    )
+import settings from '../../assets/images/settings.png';
+const MyHeader = () => {
+  return (
+    <header className="w-[90%] h-[6vh] flex align-center bg-[rgba(var(--azul-escuro),0.35)] rounded-lg fixed min-h-5 max-h-full mt-4">
+        <div className='flex w-[20%]'><img src={settings} alt="" /></div>
+        <div className='flex w-[80%] justify-center gap-25 items-center '>
+            <a href="" className='text-white'>HOME</a>
+            <a href="" className='hover:text-white text-gray-500 transition duration-200'>ABOUT ME</a>
+            <a href="" className='hover:text-white text-gray-500 transition duration-200'>COTACT</a>
+            <a href="" className='hover:text-white text-gray-500 transition duration-200'>SKILLS</a>
+            <a id='CV' className='text-white ' href="">MY CV</a>
+        </div>
+    </header>
+  )
 }
 export default MyHeader;
